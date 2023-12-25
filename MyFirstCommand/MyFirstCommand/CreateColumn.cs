384f29ea584b5bb1a3c948eb31b709cd93504bfc
huypham0808 <phamngohoanghuy95@gmail.com>
2023-12-25 22:19:36 +0700

    WPF create column

---
+++ MyFirstCommand/MyFirstCommand/CreateColumn.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.Attributes;
+
+namespace MyFirstCommand
+{
+    [TransactionAttribute(TransactionMode.Manual)]
+    public class CreateColumn : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            UIDocument uiDoc = commandData.Application.ActiveUIDocument;
+            Document doc = uiDoc.Document;
+            WPFCreateColumn creatColumnWindow = new WPFCreateColumn();
+            creatColumnWindow.Show();
+            try
+            {
+                //Phai dat qua trinh thay doi trong Transaction
+                using (Transaction trans = new Transaction(doc, "Change Element"))
+                {
+                    trans.Start();
+
+                    trans.Commit();
+                }
+                return Result.Succeeded;
+            }
+            catch (Exception ex)
+            {
+                return Result.Failed;
+            }
+        }
+    }
+}
---
+++ MyFirstCommand/MyFirstCommand/WPFUtils.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace MyFirstCommand
+{
+    public class WPFUtils
+    {
+        public static void AddListToWPFCombobox(List<string> list, ComboBox cbb)
+        {
+            cbb.ItemsSource = list;
+            cbb.SelectedIndex = 0;
+        }
+    }
+}
